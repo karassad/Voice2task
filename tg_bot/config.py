b3a880191d0b6128964980f2375b494550b7d0a2
AlexDolg <kladmoy04@mail.ru>
2025-06-14 07:17:37 +0300

    fix path to tokens

---
+++ tg_bot/config.py
@@ -0,0 +1,13 @@
+"""
+Файл конфигурации.
+
+Хранит настройки, например, токен Telegram-бота.
+"""
+
+
+BOT_TOKEN = '7639182471:AAEgPZ0GEdnGMd7EYlXH0jc7CSUUobXgGZE'
+
+# tg_bot/config.py
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+TOKENS_DIR = os.path.join(BASE_DIR, "tokens")
