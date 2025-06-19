5299cd4388c6bdaf4f06d22600cd3b4e1e721b9a
AlexDolg <kladmoy04@mail.ru>
2025-06-19 18:41:25 +0300

    loggs in oauth

--- tg_bot/utils/oauth.py
+++ tg_bot/utils/oauth.py
@@ -22,25 +22,31 @@ def generate_google_auth_url(user_id: int) -> str:
        Возвращает:
            str: Ссылка на страницу авторизации Google (user -> browser)
     """
+    try:
+        print(f"REDIRECT_URL = {RAILWAY_REDIRECT}")
 
-    print(f"REDIRECT_URL = {RAILWAY_REDIRECT}")
+        flow = Flow.from_client_secrets_file(
+            client_config=creds_dict['web'],  # файл, в котором лежат client_id, client_secret и redirect_uri
+            scopes=SCOPES,
+            redirect_uri = RAILWAY_REDIRECT
+        )
 
-    flow = Flow.from_client_secrets_file(
-        creds_dict['web'],  # файл, в котором лежат client_id, client_secret и redirect_uri
-        scopes=SCOPES,
-        redirect_uri = RAILWAY_REDIRECT
-    )
 
-    # генерим ссылку
-    #дополнительные параметры, которые настраивают поведение авторизации Google OAuth 2.0
-    auth_url, _ = flow.authorization_url(
-        access_type="offline", #чтобы получить refresh_token (позволяет работать без повторной авторизации)
-        include_granted_scopes="true", # не запрашивать заново уже одобренные права у клиента
-        prompt="consent",  #заставляет Google показать экран авторизации (даже если уже вошёл) чтобы мы точно получили refresh token
-        state=str(user_id)
-    )
+        # генерим ссылку
+        #дополнительные параметры, которые настраивают поведение авторизации Google OAuth 2.0
+        auth_url, _ = flow.authorization_url(
+            access_type="offline", #чтобы получить refresh_token (позволяет работать без повторной авторизации)
+            include_granted_scopes="true", # не запрашивать заново уже одобренные права у клиента
+            prompt="consent",  #заставляет Google показать экран авторизации (даже если уже вошёл) чтобы мы точно получили refresh token
+            state=str(user_id)
+        )
 
-    return auth_url
+        return auth_url
+    except Exception as e:
+        import traceback
+        print("❌ Ошибка при генерации ссылки авторизации:")
+        traceback.print_exc()
+        raise e
 
 
 
